fded4e86eb0380750513154a62936d64c1396b56
Jonathan Owens <jo@jondionowens.com>
2018-11-29 00:07:33 -0800

    Finished initial implementation of seed script

--- database/seed.js
+++ database/seed.js
@@ -8,22 +8,43 @@ const PropertySchema = new Schema({
 });
 
 const Property = mongoose.model('Property', PropertySchema);
+const db = mongoose.model('Property');
 
-const property1 = new Property({
-  id: '3164',
-  photos: [{
-    id: '334824',
-    location: 'http://www.thisiswherethephotogo.com'
-  },
-  {
-    id: '0108',
-    location: 'http://www.tyolo.com'
+// Helpers to generate 100 records
+let generateProperties = (qty) => {
+  const properties = [];
+  for (let i = 0; i < qty; i++) {
+    const id = generateRandomId();
+    const photos = generatePhotos(1);
+    const property = {
+      id:  '' + id,
+      photos: photos
+    }
+    properties.push(property)
   }
-]
-});
+  return properties;
+}
 
+let generatePhotos = (qty) => {
+  const photos = [];
+  for (let i = 0; i < qty; i++) {
+    const id = generateRandomId();
+    const location = 'https://picsum.photos/800/800/?image=172';
+    const newPhoto = {
+      id: '' + id,
+      location: location
+    }
+    photos.push(newPhoto);
+  }
+  return photos;
+}
 
+let generateRandomId = () => {
+  return Math.floor(Math.random() * 90000) + 10000;
+}
 
-
-
-property1.save().then(() => console.log('Records created'));
+// And finally, insert the records
+const seedProperties = generateProperties(100);
+db.insertMany(seedProperties).then(() => {
+    console.log('Done insertMany');
+  })
